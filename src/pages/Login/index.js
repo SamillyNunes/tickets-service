3942eb5747cca6d9af3154aa4fe68af474e7b9aa
Samilly <samillynunes19@gmail.com>
2024-04-20 14:55:25 -0300

    ui: login page structure and style added

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,42 @@
+import { useState } from 'react';
+
+import './login.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
 export default function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <div>
-            <h1> Pagina Login </h1>
+        <div className='container-center' >
+            <div className='login' >
+                <div className='login-area' >
+                    <img src={logo} alt='Logo do sistema de chamados' />
+                </div>
+
+                <form>
+                    <h1>Entrar</h1>
+
+                    <input 
+                        type='email' 
+                        placeholder='email@email.com' 
+                        value={email}
+                        onChange={(e)=>setEmail(e.target.value)}
+                    /> 
+                    
+                    <input 
+                        type='password' 
+                        placeholder='*******' 
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                    /> 
+
+                    <button type='submit'> Acessar </button>
+                </form>
+
+                <Link to="/register" >Criar uma conta</Link>
+            </div>
         </div>
     );
 }
