7646e2ab4242876d54c40f3f7e98fd40f106823e
Samilly <samillynunes19@gmail.com>
2024-04-20 18:04:51 -0300

    dashboard page created

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
+import Dashboard from "../pages/Dashboard";
 
 function RoutesApp(){
     return (
         <Routes>
             <Route path="/" element={ <Login/> } />
             <Route path="/register" element={ <SignUp/> } />
+            <Route path="/dashboard" element={ <Dashboard/> } />
         </Routes>
     );
 }
