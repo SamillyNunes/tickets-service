5b52466cc320e79419a661bb4b24a881a63cc3d3
Samilly <samillynunes19@gmail.com>
2024-04-22 18:42:06 -0300

    ui: title component created

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,18 @@
+import { FiSettings } from 'react-icons/fi';
+import Header from '../../components/Header';
+import Title from '../../components/Title';
+import './profile.css';
+
+export default function Profile(){
+    return (
+        <div>
+            <Header/>
+
+            <div className='content'>
+                <Title name="Minha conta" >
+                    <FiSettings size={25} />
+                </Title>
+            </div>
+        </div>
+    );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
 import Private from "./Private";
+import Profile from "../pages/Profile";
 
 function RoutesApp(){
     return (
@@ -11,6 +12,7 @@ function RoutesApp(){
             <Route path="/" element={ <Login/> } />
             <Route path="/register" element={ <SignUp/> } />
             <Route path="/dashboard" element={ <Private> <Dashboard/> </Private> } />
+            <Route path="/profile" element={ <Private> <Profile/> </Private> } />
         </Routes>
     );
 }
